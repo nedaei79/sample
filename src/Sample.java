4b0b79c85a78997ac9e5265c1bb6fb244896372e
nedaei79 <nedaei79@gmail.com>
2020-03-15 18:51:27 +0330

    first commit

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Sample {
     public static void main (String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
     }
 }
